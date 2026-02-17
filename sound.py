9d60721d3a39b4b21d756a8d34548d78045d7a3a
Anchit3034 <anchit03034@gmail.com>
2026-02-17 08:26:10 +0530

    sound .V2

--- sound.py
+++ sound.py
@@ -2,9 +2,10 @@ import numpy as np
 import sounddevice as sd
 def tonality(fs = 44100,duration =2,frequency = 21000):
     t = np.linspace(0,duration,int(fs * duration), False)
-    return 0.8 * np.sin(2 * np.pi * frequency * t),fs
+    return np.float32(0.8 * np.sin(2 * np.pi * frequency * t)),fs
 tone,fs= tonality()
-print("Playing 1000Hz tone...")
-sd.play(tone, fs)
-sd.wait()
-print("Done.")
+if __name__ == "__main__":
+    # This only runs if you play sound.py directly
+    tone, fs = tonality()
+    sd.play(tone, fs)
+
