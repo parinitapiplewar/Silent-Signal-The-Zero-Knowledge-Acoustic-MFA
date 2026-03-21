8744934f17474033318329db63c990f13403a45d
Anchit3034 <anchit03034@gmail.com>
2026-03-21 17:05:12 +0530

    hmac mod v2.0

--- hash_mess_auth.py
+++ hash_mess_auth.py
@@ -1,17 +1,10 @@
 import hmac
 import hashlib
 from sound import tonality
+SECRET_KEY=b"silent_signal_key"
 
-if __name__=="__main__":
-    secret=tonality()
-    secret = str(secret)
-
-    # Encode secret 
-    byte_secret = secret.encode('utf-8')
-
-    # Create the HMAC object and get the hexadecimal digest
-    h = hashlib.new('sha256')
-    h.update(byte_secret)
-    signature = h.hexdigest()
-
-    print(f"Digest is: {signature}")
+def generate_hmac(challenge):
+    return hmac.new(SECRET_KEY,str(challenge).encode(),hashlib.sha256).hexdigest()
+def verify_hmac(challenge,reveived):
+    expected=generate_hmac(challenge)
+    return hmac.compare_digest(expected,reveived)
